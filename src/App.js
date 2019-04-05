d2afddbb113b7f95f8a5d9d8103b38e31019a431
Jose Peteros <jose.peteros@mbww.com>
2019-04-05 13:15:04 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Table , Button} from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App container">
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              <th>Rating</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Book title</td>
+              <td> 2.5</td>
+              <td>
+                <Button color="success" size="sm" className="mr-2"> Edit</Button>
+                <Button color="danger" size="sm"> Delete</Button>
+              
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+        
       </div>
     );
   }
